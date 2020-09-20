a3f1334133dcd30701572b0b9b1fd66a9a45a7f0
yatiyr <dereeren@yahoo.com>
2020-09-20 23:10:45 +0300

    drag and zoom events added.

--- src/TestComponent/Canvas.tsx
+++ src/TestComponent/Canvas.tsx
@@ -2,16 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { TestComponentProps } from "./TestComponent.types";
+import Point from "../Utils/Point";
 
 interface State {
+  dragging: boolean;
+  selecting: boolean;
+  clickPoint: Point;
+  upperLeftPoint: Point;
+  zoomLevel: number;
+};
 
-}
-export default class Canvas extends React.Component<TestComponentProps> {
+export default class Canvas extends React.Component<TestComponentProps,State> {
 
   public node: Element | Text;
 
+  public zoom: {
+    value: number;
+    max: number;
+    step: number;
+    isStopped: boolean;
+  }
+
   constructor(props) {
     super(props);
+
+
+    this.state = {
+      zoomLevel: 100,
+      dragging: false,
+      selecting: false,
+      clickPoint: new Point(0,0),
+      upperLeftPoint: new Point(0,0)
+    }
+
+    this.zoom = {
+      value: 0,
+      max: 100,
+      step: 5,
+      isStopped: true
+    }
+
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.dragOnMouseUp = this.dragOnMouseUp.bind(this);
+    this.handleZoom = this.handleZoom.bind(this);
+    this.dragOnMouseUp = this.dragOnMouseUp.bind(this);
+    this.dragOnMouseMove = this.dragOnMouseMove.bind(this);
+    this.clearZoom = this.clearZoom.bind(this);
+
+
   }
 
   preventDefault(event: Event) {
@@ -19,29 +57,120 @@ export default class Canvas extends React.Component<TestComponentProps> {
     
   }
 
-
-  handleZoom(event: WheelEvent) {
-    event.preventDefault();
-    console.log(event.deltaY);
+  clearZoom() {
+    this.zoom.value = 1;
   }
 
+
+  // TODO: DUZELT
+  handleZoom(event: WheelEvent) {
+    event.preventDefault();
+    if((event.deltaY < 0 && this.state.zoomLevel <= 300)) {
+      this.zoom.value = this.zoom.value >= this.zoom.max ? this.zoom.max : this.zoom.value + this.zoom.step;
+      this.setState({zoomLevel: this.state.zoomLevel + this.zoom.value});
+
+    }
+    else if(event.deltaY > 0 && this.state.zoomLevel >= 25) {
+      this.zoom.value = this.zoom.value >= this.zoom.max ? this.zoom.max : this.zoom.value + this.zoom.step;
+      this.setState({zoomLevel: this.state.zoomLevel - this.zoom.value});
+    }
+
+    let xDiff = event.offsetX/10;
+    let yDiff = event.offsetY/10;
+
+    let newPoint = new Point(this.state.upperLeftPoint.x - xDiff, this.state.upperLeftPoint.y - yDiff);
+
+    this.setState({upperLeftPoint: newPoint})
+    setTimeout(this.clearZoom, 10)
+    console.log(event.offsetX, ' ', event.offsetY);
+  }
+
+  handleClick(event: MouseEvent) {
+    console.log(event.button);
+  }
+  
+  handleMouseDown(event: MouseEvent) {
+    event.preventDefault();
+    // If left click happened
+    if(event.button === 0) {
+      this.setState({selecting: true, clickPoint: new Point(event.offsetX,event.offsetY)});
+    }
+    // If middle click happened
+    else if(event.button === 1) {
+      this.setState({dragging: true, clickPoint: new Point(event.offsetX,event.offsetY)});
+    }
+    // If right click happened
+    else if(event.button === 2) {
+      console.log("right clicked!") // TODO: IMPLEMENT LATER
+    }
+  }
+
+  dragOnMouseMove(event: MouseEvent) {
+    event.preventDefault();
+    let zoomFactor = parseFloat((1/(this.state.zoomLevel/100)).toFixed(2))
+    let diagramDragSpeed = new Point(1,1);
+
+    diagramDragSpeed.x *= zoomFactor;
+    diagramDragSpeed.y *= zoomFactor;
+
+    console.log(diagramDragSpeed.x, ' ', diagramDragSpeed.y);
+
+    let newPoint = new Point(this.state.upperLeftPoint.x + event.movementX*diagramDragSpeed.x, 
+                             this.state.upperLeftPoint.y + event.movementY*diagramDragSpeed.y);
+    
+    //newPoint.x *= zoomFactor;
+    //newPoint.y *= zoomFactor;
+
+
+    this.setState({upperLeftPoint: newPoint});
+
+    if(!this.node.contains(event.target as Node)) {
+      console.log("dragFinished");
+      this.setState({dragging: false});
+    }
+  }
+
+  dragOnMouseUp(event: MouseEvent) {
+    console.log("dragFinished");
+    this.setState({dragging: false});
+  }
+
+  componentDidUpdate(props,state) {
+    if(this.state.dragging && !state.dragging) {
+      window.addEventListener('mousemove', this.dragOnMouseMove);
+      window.addEventListener('mouseup', this.dragOnMouseUp);
+    }
+    else if(!this.state.dragging && state.dragging) {
+      window.removeEventListener('mousemove', this.dragOnMouseMove);
+      window.removeEventListener('mouseup', this.dragOnMouseUp);
+    }
+    else if(this.state.selecting && !state.selecting) {
+
+    }
+    else if(!this.state.selecting && state.selecting) {
+
+    }
+  }
 
   componentDidMount() {
     this.node = ReactDOM.findDOMNode(this);
     this.node.addEventListener("contextmenu", this.preventDefault);
     this.node.addEventListener("wheel",this.handleZoom);
+    this.node.addEventListener("mousedown",this.handleMouseDown);
   }
 
   componentWillUnmount() {
-    this.node.removeEventListener("contextmenu", this.preventDefault)
+    this.node.removeEventListener("contextmenu", this.preventDefault);
+    this.node.removeEventListener("wheel",this.handleZoom);
+    this.node.removeEventListener("mousedown",this.handleClick);
   }
   
   render() {
     return(
 
       <div className="diagramWrapper">
-        <svg width="200" height="200" version="1.1" shapeRendering="geometricPrecision">
-          <g transform="scale(1.5) translate(30,30)">
+        <svg width="100%" height="100%" version="1.1" shapeRendering="geometricPrecision">
+          <g transform={`scale(${this.state.zoomLevel/100}) translate(${this.state.upperLeftPoint.x},${this.state.upperLeftPoint.y})`}>
             <path fill= "none" 
               stroke="transparent" 
               strokeWidth="7"
--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -21,14 +21,4 @@ class Comp extends React.Component<TestComponentProps> {
   }
 }
 
-const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
-  <div
-    data-testid="test-component"
-    className={`test-component test-component-${theme}`}
-  >
-    <h1 className="heading">I'm the test component</h1>
-    <h2>Made with love by Harvey</h2>
-  </div>
-);
-
 export default Comp;
